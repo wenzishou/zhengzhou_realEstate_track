0e1150023c0f0723b50c3c42bac0361a55ca690e
wenzishou <lliumengtao@126.com>
2018-04-15 03:14:14 -0400

    modify data path

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import datetime
 import main_process
 import json
 start_url = 'http://www.mizhai.com/'
-write_dir = ''
+write_dir = './data/'
 if __name__ == '__main__':
     left_url = set()
     visited_url = set()
@@ -22,8 +22,9 @@ if __name__ == '__main__':
         if to_url in visited_url:
             continue
         info, ref_url = main_process.collect_info(to_url)
-        print(type(json.dumps(info, encoding="UTF-8", ensure_ascii=False).encode('utf-8')))
+        #print(type(json.dumps(info, encoding="UTF-8", ensure_ascii=False).encode('utf-8')))
         file_obj.write((json.dumps(info, encoding="UTF-8", ensure_ascii=False).encode('utf-8')) + '\n')
         left_url = left_url | ref_url
         visited_url.add(to_url)
-        print(len(left_url), len(visited_url))
+        #print(len(left_url), len(visited_url))
+    print(time_str, len(visited_url))
